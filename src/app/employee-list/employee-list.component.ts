6118566ad86038d9d51aaf7d1f492df7976e8402
yasidew <it21440922@my.sliit.lk>
2023-07-04 18:31:41 +0530

    changes made

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -34,7 +34,11 @@ export class EmployeeListComponent implements  OnInit {
     this.router.navigate(['update-employee', id]);
   }
   deleteEmployee(id: number){
-    
+    this.employeeService.deleteEmployee(id).subscribe(data => {
+      console.log(data);
+      this.getEmployees();
+    })
+
   }
 
   }
