72898c9aa179e76226f37d6b394820783846fd3e
yasidew <it21440922@my.sliit.lk>
2023-07-04 19:21:28 +0530

    committed

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { ActivatedRoute, Route } from '@angular/router';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.css']
 })
-export class EmployeeDetailsComponent {
+export class EmployeeDetailsComponent implements  OnInit{
+
+  id:number = 0;
+  employee: Employee = new Employee();
+  constructor(private route: ActivatedRoute, private employeeService: EmployeeService){}
+
+  ngOnInit(): void {
+      this.id = this.route.snapshot.params['id']; // get the id from the url
+
+      this.employee = new Employee();
+      this.employeeService.getEmployeeById(this.id).subscribe(data => {
+        this.employee = data;
+      })
+  }
 
 }
